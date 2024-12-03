89ef786ed9d3ffbaff1ffca10c3c4595a9ff6b12
Hezzie <hezronchelimo.hc@gmail.com>
2024-12-04 00:18:24 +0300

    added a responsive navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-4 py-2 m-0 h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-4 py-2 m-0 min-h-screen flex flex-col`}
       >
-        {children}
+        {/* Fixed Navbar */}
+        <nav className="fixed bottom-0  sm:top-0 md:top-0  lg:bottom-0  h-[60px]  w-full bg-white p-4 z-10">
+          <ul className="flex space-x-6">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        {/* Main Content */}
+        <div className="flex-1 pt-16 pb-20">{children}</div>
       </body>
     </html>
   );
